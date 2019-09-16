a999b0ce9ca539f3858cc05d7e345b74d5add348
krishna chittodia <itadmin@lapsun15.successivetech.com>
2019-09-16 16:49:41 +0530

    task3 commit

---
+++ extra/patterns/diamond.js
@@ -0,0 +1,29 @@
+let rows = process.argv.slice(2)[0];
+let k, i, j;
+let pattern1 = "";
+for (i = 1; i <= rows; i++) {
+  for (j = 1; j <= rows - i; j++) {
+    pattern1 += " ";
+  }
+  for (k = 1; k <= i; k++) {
+    pattern1 += "*";
+    pattern1 +=" ";
+  }
+  console.log(pattern1);
+  pattern1 = "";
+
+}
+
+let pattern2 = "";
+for (let i = rows; i >= 1; i--) {
+  for (let j = i; j <= rows - 1; j++) {
+    pattern2 += " ";
+  }
+  for (let k = 1; k <= i; k++) {
+    pattern2 += "*";
+    pattern2 += " "
+  }
+  console.log(pattern2);
+  pattern2 = "";
+
+}
---
+++ extra/patterns/equilateral.js
@@ -0,0 +1,13 @@
+let rows=process.argv.slice(2)[0];
+
+
+for (var i = 0; i < rows; i++) {
+    var str = '';
+    for (var j = 1; j < rows-i; j++) {
+      str = str + ' ';
+    }
+    for (var k = 1; k <= (2*i+1); k++) {
+      str = str + '*';
+    }
+    console.log(str);
+} 
