b59e4518f44770675ba28f85f4c052314805188e
krishna chittodia <itadmin@lapsun15.successivetech.com>
2019-09-24 12:13:14 +0530

    discard changes

--- src/libs/routes/errorHandler.ts
+++ src/libs/routes/errorHandler.ts
@@ -1,11 +1,11 @@
 export default function errorHandler(err, req, res, next) {
     console.log('inside errorHandler');
-    const { status, message, error} = err;
+    const { error, status, message } = err;
     const timestamp: Date = new Date();
     res.status(status).send({
         error: error || 'Not Found',
-        message: message || 'Bad Request',
-        status: status || 400,
+        message: message || 'error',
+        status: status || 500,
         timestamp,
     });
 }
